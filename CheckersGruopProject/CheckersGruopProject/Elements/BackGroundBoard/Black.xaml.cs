da3854b25263d8181ee911fa76a8d58e2ef2fb82
murizzi67 <murizzi.simone.23@itisriva.edu.it>
2026-05-01 16:45:46 +0200

    Add files via upload

--- CheckersGruopProject/CheckersGruopProject/Elements/BackGroundBoard/Black.xaml.cs
+++ CheckersGruopProject/CheckersGruopProject/Elements/BackGroundBoard/Black.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,18 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing;
 
-namespace CheckersGruopProject
+namespace CheckersGruopProject.Elements.BackGroundBoard
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per Black.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Black : Window
     {
-        public MainWindow()
+        public Black()
         {
             InitializeComponent();
         }
